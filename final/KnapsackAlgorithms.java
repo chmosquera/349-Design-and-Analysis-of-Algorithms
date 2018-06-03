f5b40fc2390f15afbd164423183cc7fdbd1f85b5
Chanelle <mosquera.ch@gmail.com>
2018-06-03 02:37:32 -0700

    working on knapsack brute force

--- final/KnapsackAlgorithms.java
+++ final/KnapsackAlgorithms.java
@@ -15,6 +15,62 @@ class KnapsackAlgorithms
 
     public KnapsackAlgorithms() {}
 
+    public static void ExhaustiveMethod(ArrayList<Integer> items, ArrayList<Integer> values, ArrayList<Integer> weights, int capacity) {
+        int n = items.size();
+
+        System.out.println("(1<<n): " + (1<<n));
+        // generate subsets
+        int[] subsets = new int[1<<n];      // contains all sets
+        for (int i = 0; i < (1<<n); i++) {
+            //System.out.println("first for loop - i: " + i);
+            
+
+//            System.out.println("i: " + i);
+            int temp = 0;
+
+            for (int j = 0; j < n; j++) {
+      //          System.out.println("\tj: " + j);
+    //            System.out.println("\t\t1<<j: " + (1<<j));
+  //              System.out.println("\t\ti&(1<<j): " + (i&(1<<j)));
+//                System.out.println("temp: " + temp);
+                    //System.out.println("j: " + j );
+                if ((i & (1<<j)) > 0) {
+//                    System.out.print(set[j] + " ");
+                    //System.out.print("temp | j = temp: " + temp + "|" + j);
+                    temp = temp | (i&(1<<j));
+                  //  System.out.println(" = " + temp);
+                    
+                } 
+            }
+//            System.out.println("\t--> set: " + Integer.toBinaryString(temp));
+  //          System.out.println("}");
+            subsets[i] = temp;
+
+        }
+
+        // iterate through subsets - find weights and values of each
+        System.out.println("printing entire set");
+        int maxvalue = 0;
+        int weight = 0;
+
+        for (int i = 0; i < (1<<n); i++) {      // loop through subsets
+            
+            System.out.println(Integer.toBinaryString(subsets[i]));
+            int v = 0;
+            int w = 0;
+            for (int j=0; j < n; j++) {
+                if ((subsets[i] & j) == j) {
+                    System.out.println("\t item " + (j + 1) + " is included");
+                    System.out.println("\t\t value: " + values.get(j) + " weight: " + weights.get(j));
+                    v += values.get(j);
+                    w += weights.get(j);
+                }
+            }
+            System.out.println("\t\t TotalValue: " + v + " TotalWeight: " + w);
+        }
+
+    }
+
     public static void main(String args[]) {
 
         if (args.length < 2) {
@@ -49,6 +105,15 @@ class KnapsackAlgorithms
             }
             System.out.println(capacity);
 
+//        ExhaustiveMethod(items, values, weights);
+        ArrayList<Integer> t_i = new ArrayList<Integer>();
+        t_i.add(1); t_i.add(2); t_i.add(3); 
+        ArrayList<Integer> t_v = new ArrayList<Integer>();
+        t_v.add(4); t_v.add(5); t_v.add(5); 
+        ArrayList<Integer> t_w = new ArrayList<Integer>();
+        t_w.add(2); t_w.add(4); t_w.add(1);
+        ExhaustiveMethod(t_i, t_v, t_w, 5);
+
 
         }
         catch(FileNotFoundException e) {
@@ -56,7 +121,6 @@ class KnapsackAlgorithms
         }
 
     }
-
 
 
 }
