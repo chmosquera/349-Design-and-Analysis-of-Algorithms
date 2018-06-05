5408229630a72e20549764f09d282a3eefdcfdea
Chanelle <mosquera.ch@gmail.com>
2018-06-04 19:10:57 -0700

    finished dynamic programming

---
+++ final/KnapsackAlgorithms_w_printing.java
@@ -0,0 +1,81 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.lang.*;
+import java.lang.Integer;
+
+// CSC 349 Final Assignment Knapsack Problems
+
+class KnapsackAlgorithms
+{
+
+    public KnapsackAlgorithms() {}
+
+    public static void main(String args[]) {
+
+        if (args.length < 2) {
+            System.out.println("Usage: KnapsackAlgorithms <file> <approach>");
+            System.out.println("(approach: 1 - enum | 2 - greedy | 3 - dyn prog | 4 - b'n'b)");
+            return;
+        }
+
+        int approachID = Integer.parseInt(args[1]);
+        File file = new File(args[0]);
+
+        try {
+
+            Scanner sc = new Scanner(file);
+            int item_cnt = sc.nextInt();            // n
+
+            ArrayList<Integer> items = new ArrayList<Integer>(item_cnt);
+            ArrayList<Integer> values = new ArrayList<Integer>(item_cnt);
+            ArrayList<Integer> weights = new ArrayList<Integer>(item_cnt);
+
+            for (int i = 0; i < item_cnt; i++) {    
+                items.add(sc.nextInt());            // items
+                values.add(sc.nextInt());           // values
+                weights.add(sc.nextInt());          // weights
+            }
+            int capacity = sc.nextInt();            // capacity
+
+            // for testing smaller arrays
+            ArrayList<Integer> t_i = new ArrayList<Integer>();
+            t_i.add(1); t_i.add(2); t_i.add(3); 
+           ArrayList<Integer> t_v = new ArrayList<Integer>();
+            t_v.add(4); t_v.add(5); t_v.add(5); 
+            ArrayList<Integer> t_w = new ArrayList<Integer>();
+            t_w.add(2); t_w.add(4); t_w.add(1);
+
+            switch (approachID) {
+                case 1:     // full enumeration
+                    KnapsackFullEnumeration fullenum = new KnapsackFullEnumeration();
+                    fullenum.BruteForce(items, values, weights, capacity);
+                    break;
+                case 2:     // greedy
+                    KnapsackGreedySearch greedy = new KnapsackGreedySearch();
+                    greedy.MaxValuePerWeight(items, values, weights, capacity);
+                    break;
+                case 3:     // dyn prog
+                    KnapsackDynProg dynprog = new KnapsackDynProg();
+                    dynprog.Solve(items, values, weights, capacity);
+                    break;
+            }
+
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Could not process file");
+        }
+
+
+//        ExhaustiveMethod(t_i, t_v, t_w, 6);
+//
+
+
+    }
+
+
+}
--- final/KnapsackDynProg.java
+++ final/KnapsackDynProg.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.lang.*;
 
-// Assignment Week 5 - Rod Cutter
+// Final Assignment - Knapsack Algorithms - Dynamic Progrmaming
 
 class KnapsackDynProg
 {
@@ -27,38 +27,51 @@ class KnapsackDynProg
         items.add(0,0);     // prepend 0 to get row/col of 0's
         values.add(0,0);
         weights.add(0,0);
+
         // bottom up approach - recursively compute the max value found for smaller caps
         //      which will be used to computer max value for larger caps
-        for (int c = 1; c < capacity; c++) {
-            for (int i = 1; i < n; i++) {
-                int v = 0;
+        for (int c = 1; c <= capacity; c++) {
+            for (int i = 1; i <= n; i++) {
                 // can we pick up this item?
-                if (weights.get(i) < c) {
+                if (weights.get(i) <= c) {
                     int w = c - weights.get(i);     // remaining weight if item picked up
-
                     // should we pick up this item?
                     if (sub_solns[i-1][c] < (values.get(i) + sub_solns[i-1][w])) {
                         sub_solns[i][c] = values.get(i) + sub_solns[i-1][w];
                     } else {
                         sub_solns[i][c] = sub_solns[i-1][c];
                     }
+                    //System.out.println("this item:\ti: " + i + "\tv: " + values.get(i) + "\tw: " + weights.get(i)  + "\tvalue thus far: " + sub_solns[i][c]);
+                    //System.out.println("prev item:\ti: " + (i-1) + "\tv: " + values.get(i-1) + "\tw: " + weights.get(i-1)  + "\tvalue thus far: " + sub_solns[i-1][c]);
+                } else {
+                    sub_solns[i][c] = sub_solns[i-1][c];
                 }
+            }
+        }
+
+        // testing
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < capacity; j++) {
+               // System.out.println("item: " + i + "\tcap: " + j + "\tvalue up to this point:" + sub_solns[i][j]);
             }
         }
 
         // trace back
         ArrayList<Integer> soln = new ArrayList<Integer>();
         int i = n, c = capacity;        // start at last item & at capacity
-        while (c > 0) {
-            if (sub_solns[n][c] == sub_solns[n-1][c]) {     // i was not picked up
-                
+       // System.out.println("i = " + n + "\tc: " + capacity  + " last solution: " + sub_solns[n][c]);
+        while (c > 0 && i > 0) {
+            if (sub_solns[i][c] == sub_solns[i-1][c]) {     // i was not picked up
+             i--;
             } else {
                 soln.add(i);
                 tot_value += values.get(i);
                 tot_weight += weights.get(i);
+                c-=weights.get(i);
+                i--;
             }
         }
-       
+      
         System.out.println("Dynamic Programming solution: Value " + tot_value + ", Weight " + tot_weight);
         System.out.println(Arrays.toString(soln.toArray()));
         
